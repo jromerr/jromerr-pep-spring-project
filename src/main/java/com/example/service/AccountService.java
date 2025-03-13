0ef03d3d071b8e4ed36e3547ab0c743588d63e5b
Jose Romero <romer243@csusm.edu>
2025-03-13 04:24:55 +0000

    Add initial account logic

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,19 @@
 package com.example.service;
 
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+    private final AccountRepository accountRepository;
+
+    public AccountService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+
+    public Account addAccount(Account account){
+        return accountRepository.save(account);
+    }
 }
