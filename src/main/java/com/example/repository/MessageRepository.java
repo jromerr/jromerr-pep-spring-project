bd0e37d39a397ca938d10c6d8feb03a7dad3b302
Jose Romero <romer243@csusm.edu>
2025-03-14 03:43:30 +0000

    Implement custom message queries

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,8 +1,20 @@
 package com.example.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.entity.Message;
 
 public interface MessageRepository extends JpaRepository<Message, Integer> {
+    @Query("FROM message WHERE postedBy = :userId")
+    List<Message> findAllByPostedBy(@Param("userId") Integer accId);
+
+    @Query("FROM message WHERE messageId = :mId")
+    Message getByMessageId(@Param("mId") Integer messageId);
+
+    @Query("FROM message WHERE messageId = :mId")
+    Message deleteByMessageId(@Param("mId") Integer messageId);
 }
