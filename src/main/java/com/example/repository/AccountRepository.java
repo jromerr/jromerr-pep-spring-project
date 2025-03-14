a49206209560db32dd0cc5597aca0204722de6e6
Jose Romero <romer243@csusm.edu>
2025-03-14 02:41:00 +0000

    Implement custom query for usernames

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,9 +1,12 @@
 package com.example.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.entity.Account;
 
 public interface AccountRepository extends JpaRepository<Account, Integer>{
-    
+    @Query("FROM account WHERE username = :user")
+    Account findByUsername(@Param("user") String username);
 }
