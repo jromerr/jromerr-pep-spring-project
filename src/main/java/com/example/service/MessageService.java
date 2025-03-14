9f8eaf62c55f67fb11115e384d5cf6008848ff95
Jose Romero <romer243@csusm.edu>
2025-03-14 03:53:28 +0000

    Rework syntax

--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -27,18 +27,18 @@ public class MessageService {
     }
 
     public Message getMessageByMessageId(int messageId){
-        return messageRepository.getByMessageId(messageId);
+        return messageRepository.findByMessageId(messageId);
     }
 
     public Message deleteMessage(int messageId){
-        if(messageRepository.getByMessageId(messageId) != null){
+        if(messageRepository.findByMessageId(messageId) != null){
             return messageRepository.deleteByMessageId(messageId);
         }
         return null;
     }
 
     public Message replaceMessage(Message message){
-        if(message.getMessageText() != "" && message.getMessageText().length() < 255 && messageRepository.getByMessageId(message.getMessageId()) != null){
+        if(message.getMessageText() != "" && message.getMessageText().length() < 255 && messageRepository.findByMessageId(message.getMessageId()) != null){
             return messageRepository.save(message);
         }
         return null;
